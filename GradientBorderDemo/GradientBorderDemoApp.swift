a00a91b52ff52c6e9ae82436435b5a741eb5cf3b
macro6461 <mattcroak718@gmail.com>
2022-05-11 11:48:44 -0400

    Initial Commit

---
+++ GradientBorderDemo/GradientBorderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientBorderDemoApp.swift
+//  GradientBorderDemo
+//
+//  Created by Matt C on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GradientBorderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
